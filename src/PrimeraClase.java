a3431858eb7a36b149a870465b7d66c5acfdc531
Yodiha <cristinamartinez48+1@gmail.com>
2024-09-19 19:57:17 +0200

    Update PrimeraClase.java desde mi silla

--- src/PrimeraClase.java
+++ src/PrimeraClase.java
@@ -3,6 +3,9 @@ public class PrimeraClase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String nombre="luz";
+		System.out.println(nombre);
+		System.out.println("hola hola3");
 
 	}
 
